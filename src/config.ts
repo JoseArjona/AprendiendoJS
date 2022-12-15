b8d26d4335c092680faa9752e440854fbda4f0af
JoseArjona <j.angel.matheis@gmail.com>
2022-12-14 20:40:54 -0600

    mejor seo

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'GuiavaScript';
-export const SITE_DESCRIPTION = 'Es una página guía que te ayudará a repasar o comprender JavaScript básico, el objetivo, no es ser un curso o una documentación, simplemente es un apoyo, una recopilación de mis conocimientos sobre este lenguaje. A medida que vayas avanzando irá aumentando el progreso.';
+export const SITE_DESCRIPTION = 'GuiavaScript es una página guía que te ayudará a repasar o comprender JavaScript básico, el objetivo, no es ser un curso o una documentación, simplemente es un apoyo, una recopilación de mis conocimientos sobre este lenguaje. A medida que vayas avanzando irá aumentando el progreso.';
